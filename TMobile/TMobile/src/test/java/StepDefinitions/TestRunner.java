64e33e3dd09822d858b224c6d49970054f903b74
Oleksandr Oleshko <oleshko10@gmail.com>
2025-06-24 09:13:03 -0700

    Test Runner

--- TMobile/TMobile/src/test/java/StepDefinitions/TestRunner.java
+++ TMobile/TMobile/src/test/java/StepDefinitions/TestRunner.java
@@ -5,7 +5,10 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/Features", glue = {"StepDefinitions"}, monochrome = true,
+@CucumberOptions(features = "src/test/resources/Features",
+        glue = {"StepDefinitions"},
+        monochrome = true,
+        tags = "@smoke",
         plugin = {"pretty", "junit:target/JUnitReports/report.xml"})
 public class TestRunner {
 
